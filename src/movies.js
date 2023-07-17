1381dc06a3c439fd93b9960d3f0160a9e30d9310
Oscar Mesejo <oscar.mesejo@gmail.com>
2023-07-17 17:40:49 +0200

    las class 06

--- src/movies.js
+++ src/movies.js
@@ -1,19 +1,47 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {}
+function getAllDirectors(moviesArray) {
+    return moviesArray.map(movie => {
+        return movie.director
+    })
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
+function howManyMovies(moviesArray) {
+    const dramaMovies = moviesArray.filter((movie)=> {
+        return movie.director.startsWith("Steven") && movie.genre.includes("drama");
+    })
+    return dramaMovies.length;
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+function scoresAverage(moviesArray) {
+    const scoreIMBD = moviesArray.map((element)=>{
+       return element.score 
+    })
+
+    const totalScore = justScore.reduce((acc, element)=>{
+        return acc + element
+    })
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesScore(moviesArray) {}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
+function orderByYear(moviesArray) {
+    const sortedArray = orderByYear.sort((number1, number2)=>{
+        if (number1 < number2){
+            return -1
+        } else if (number1 > number2){
+            return 1
+        } else{
+            return 0
+        }
+    })
+    console.log(sortedArray);
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically(moviesArray) {}
